c0f868141e25ff8a23b20f5c4d34af96e5aa005f
tennjugu <133909875+tennjugu@users.noreply.github.com>
2023-09-06 18:14:04 +0100

    remove coented code and empty spaces

--- index.js
+++ index.js
@@ -28,70 +28,62 @@ function bookItem(){
     for (let i = 0; i < myLibrary.length; i++){
         const book = myLibrary[i]
 
-    const eachBookDiv = document.createElement("div")
-    eachBookDiv.classList.add("eachBookDiv")
+        const eachBookDiv = document.createElement("div")
+        eachBookDiv.classList.add("eachBookDiv")
 
-    const bookDetails = document.createElement("div")
-    bookDetails.classList.add("book-details")
-    
+        const bookDetails = document.createElement("div")
+        bookDetails.classList.add("book-details")
+        
 
-    const bookTitle = document.createElement("h3")
-    bookTitle.classList.add("book-title")
-    bookTitle.textContent = `${book.title}`
-    bookDetails.appendChild(bookTitle)
+        const bookTitle = document.createElement("h3")
+        bookTitle.classList.add("book-title")
+        bookTitle.textContent = `${book.title}`
+        bookDetails.appendChild(bookTitle)
 
-    const bookAuthor = document.createElement("h5")
-    bookAuthor.classList.add("book-author")
-    bookAuthor.textContent = `by: ${book.author}`
-    bookDetails.appendChild(bookAuthor)
+        const bookAuthor = document.createElement("h5")
+        bookAuthor.classList.add("book-author")
+        bookAuthor.textContent = `by: ${book.author}`
+        bookDetails.appendChild(bookAuthor)
 
-    const bookPages = document.createElement("h6")
-    bookPages.classList.add("book-pages")
-    bookPages.textContent = `${book.pages} page(s)`
-    bookDetails.appendChild(bookPages)
-
-
-    const readStatus = document.createElement("h6")
-    readStatus.classList.add("read-status")
-    readStatus.textContent = book.read ? "Read" : "Not Read"
-    bookDetails.appendChild(readStatus)
+        const bookPages = document.createElement("h6")
+        bookPages.classList.add("book-pages")
+        bookPages.textContent = `${book.pages} page(s)`
+        bookDetails.appendChild(bookPages)
 
 
-    const bookBtns = document.createElement("div")
-    bookBtns.classList.add = "book-btns"
-   
-
-    const removeButton = document.createElement("button")
-    removeButton.classList.add("remove-button")
-    removeButton.textContent = "Remove Book"
-    removeButton.addEventListener("click", function() {
-        removeBook(i)
-    })
-    bookBtns.appendChild(removeButton)
+        const readStatus = document.createElement("h6")
+        readStatus.classList.add("read-status")
+        readStatus.textContent = book.read ? "Read" : "Not Read"
+        bookDetails.appendChild(readStatus)
 
 
-    const toggleButton = document.createElement("button")
-    toggleButton.classList.add("toggle-button")
-    toggleButton.textContent = "Toggle Status"
-    toggleButton.addEventListener("click", function() {
-        toggle(i)
-    })
-    bookBtns.appendChild(toggleButton)
+        const bookBtns = document.createElement("div")
+        bookBtns.classList.add = "book-btns"
 
-    bookDetails.appendChild(bookBtns)
-    eachBookDiv.appendChild(bookDetails)
 
-    eachBook.appendChild(eachBookDiv)
+        const removeButton = document.createElement("button")
+        removeButton.classList.add("remove-button")
+        removeButton.textContent = "Remove Book"
+        removeButton.addEventListener("click", function() {
+            removeBook(i)
+        })
+        bookBtns.appendChild(removeButton)
+
+
+        const toggleButton = document.createElement("button")
+        toggleButton.classList.add("toggle-button")
+        toggleButton.textContent = "Toggle Status"
+        toggleButton.addEventListener("click", function() {
+            toggle(i)
+        })
+        bookBtns.appendChild(toggleButton)
+
+        bookDetails.appendChild(bookBtns)
+        eachBookDiv.appendChild(bookDetails)
+
+        eachBook.appendChild(eachBookDiv)
+    }
 }
-}
-
-// function bookItem(){
-    
-    
-//     bookItem(book)
-    
-//     }
-// }
 
 function addBookToLibrary() {
     let title = document.querySelector("#title").value
